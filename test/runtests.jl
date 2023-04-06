40b40cd267f88ad4dd7eb3754d84f1ee7db10dfc
Nils Niggemann <nils.niggemann@fu-berlin.de>
2023-04-06 13:24:08 +0200

    added unit tests

---
+++ test/runtests.jl
@@ -0,0 +1,28 @@
+using CifToSpinFRGLattice,Test
+
+@testset "parseSymmetries" begin
+    symlist = [
+        "'   '-z, -x, y''"
+        "'   '-z+1/4, -x-0.1, y*2''"
+    ]
+    syms = modifySymmetries!(symlist)
+    @test syms == [
+        "r-> SA[-r[3],-r[1],r[2]]"
+        "r-> SA[-r[3]+1/4,-r[1]-0.1,r[2]*2]"
+    ]
+    syms = getSymmetries(syms)
+    x,y,z = r = [1,2,3]
+    @test syms[1](r) == [-3,-1,2]
+    @test syms[2](r) == [-z+1/4, -x-0.1, y*2]
+end
+
+@testset "reading Symmetries" begin
+    syms = getSymmetries("SimpleCubic.cif")
+    @test length(syms) == 12
+    
+    x,y,z = r = [10.5,11.2,-10.4]
+    r´ = [-y, -z, x]
+
+    @test syms[1](r) == r # identity
+    @test syms[end](r) == r´
+end
